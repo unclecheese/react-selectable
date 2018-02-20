f620cacf8fe26edcc9c675efa5f643eced495c60
Aslam Ahammed <aslamplr@users.noreply.github.com>
2018-02-20 13:16:39 +0530

    type definition for react-selectable

---
+++ react-selectable.d.ts
@@ -0,0 +1,30 @@
+
+declare module 'react-selectable' {
+    interface ReactSelectableGroupProps {
+        onSelection?: (selectedItems: Array<any>) => void;
+        onNonItemClick?: () => void;
+        tolerance?: number;
+        component?: string;
+        fixedPosition?: boolean;
+        selectOnMouseMove?: boolean;
+        preventDefault?: boolean;
+        enabled?: boolean;
+        [key: string]: any;
+    }
+
+    interface ReactSelectableComponentProps {
+        key: number|string;
+        selected: boolean;
+        selectableKey: number|string;
+        [key: string]: any;
+    }
+
+    export class SelectableGroup extends React.Component<ReactSelectableGroupProps> {
+
+    }
+
+    class SelectableComponent extends React.Component<ReactSelectableComponentProps> {
+
+    }
+    export const createSelectable: (component: React.ReactNode) => typeof SelectableComponent;
+}
